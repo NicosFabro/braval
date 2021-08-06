99b9fb893a5b7e033e2c1c9fe897bdc53a3b8277
nicosfabro <nicosfabroc@gmail.com>
2021-08-06 16:49:31 +0200

    Made Authentication and Cache repositories

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+import 'package:cache/cache.dart';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:authentication_repository/src/exceptions/exceptions.dart';
+
+class AuthenticationRepository {
+  AuthenticationRepository({
+    CacheClient? cache,
+    firebase_auth.FirebaseAuth? firebaseAuth,
+  })  : _cache = cache ?? CacheClient(),
+        _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+
+  final CacheClient _cache;
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+
+  @visibleForTesting
+  static const userCacheKey = '__user_cache_key__';
+
+  Stream<User> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      final user = firebaseUser == null ? User.empty : firebaseUser.toUser;
+      _cache.write(key: userCacheKey, value: user);
+      return user;
+    });
+  }
+
+  User get currentUser {
+    return _cache.read<User>(key: userCacheKey) ?? User.empty;
+  }
+
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw SignUpFailure();
+    }
+  }
+
+  Future<void> logInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw LogInWithEmailAndPasswordFailure();
+    }
+  }
+
+  Future<void> logOut() async {
+    try {
+      await Future.wait([
+        _firebaseAuth.signOut(),
+      ]);
+    } on Exception {
+      throw LogOutFailure();
+    }
+  }
+}
+
+extension on firebase_auth.User {
+  User get toUser {
+    return User(id: uid, email: email);
+  }
+}
